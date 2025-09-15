eff3d42c1ad8a0862eca15b4a240babd04532432
Justus Perlwitz <hello@justus.pw>
2025-09-15 09:27:06 +0900

    Add adapter-static

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 // svelte.config.js
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter(),
+        adapter: adapter({ fallback: "200.html" }),
     },
     preprocess: [vitePreprocess()],
 };
